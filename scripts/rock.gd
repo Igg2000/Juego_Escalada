fc119fb55509b8d8cce28ba55959992a59360983
LightningFast <bydraxx4k129@gmail.com>
2025-02-23 16:29:44 +0100

    v0.0.24
    He agreado la mecanica que hace que cambie de Sprite el brazo al estar dentro del Area2D de la roca para que al pulsar el Shift, cambie a agarrando la roca

--- scripts/rock.gd
+++ scripts/rock.gd
@@ -1,24 +1,57 @@
 extends Node2D
 
+var puede_agarrarse = false
+@export var textura_normal_BrazoIz: Texture2D
+@export var textura_agarrado_BrazoIz: Texture2D
+@export var textura_normal_BrazoDr: Texture2D
+@export var textura_agarrado_BrazoDr: Texture2D
+var brazo_izquierdo: Sprite2D = null
+var brazo_derecho: Sprite2D = null
+
 func _ready():
-	print("Script de Rock iniciado")  # Verifica que el script se esté ejecutando
 	if has_node("Area2D"):
-		print("Area2D encontrado en Rock")  # Verifica que el Area2D exista
-		$Area2D.connect("area_entered", Callable(self, "_on_body_entered"))
-		$Area2D.connect("area_exited", Callable(self, "_on_body_exited"))
+		$Area2D.connect("area_entered", Callable(self, "_on_area_entered"))
+		$Area2D.connect("area_exited", Callable(self, "_on_area_exited"))
 	else:
-		print("Error: No se encontró el nodo Area2D en Rock")  # Mensaje de error
+		printerr("Error: No se encontró Area2D en la roca")
 
-func _on_body_entered(body):
-	#print("Cuerpo detectado:", body.name)  # Verifica qué cuerpo está entrando
-	if body.is_in_group("AntebrazoIc"):
-		print("Brazo cerca de la roca")
-	else:
-		print("Otro cuerpo detectado:", body.name)  # Verifica si otros cuerpos están entrando
+func _on_area_entered(area):
+	#Verificamos si el área pertenece a los brazos
+	if area.is_in_group("AntebrazoIc") || area.is_in_group("AntebrazoDc"):
+		#Obtenemos la jerarquía completa: Area2D -> Antebrazo -> base -> Jugador
+		if area.get_parent().is_in_group("AntebrazoIc"):
+			brazo_izquierdo = area.get_parent()
+		if area.get_parent().is_in_group("AntebrazoDc"):
+			brazo_derecho = area.get_parent()
+		
+		puede_agarrarse = true
+		#print("Brazo detectado cerca de la roca")
 
-func _on_body_exited(body):
-	#print("Cuerpo que sale:", body.name)  # Verifica qué cuerpo está saliendo
-	if body.is_in_group("AntebrazoIc"):
-		print("Brazo se aleja de la roca")
+func _on_area_exited(area):
+	if area.is_in_group("AntebrazoIc") || area.is_in_group("AntebrazoDc"):
+		puede_agarrarse = false
+		resetear_texturas()
+		#print("Brazo se aleja de la roca")
+
+func _input(event):
+	if event is InputEventKey:
+		if event.pressed && event.keycode == KEY_SHIFT:
+			if puede_agarrarse:
+				cambiar_textura_agarre(true)
+			else:
+				cambiar_textura_agarre(false)
+
+func cambiar_textura_agarre(agarrado: bool):
+	if agarrado:
+		if brazo_izquierdo: brazo_izquierdo.texture = textura_agarrado_BrazoIz
+		if brazo_derecho: brazo_derecho.texture = textura_agarrado_BrazoDr
+		print("¡Agarrándose a la roca!")
 	else:
-		print("Otro cuerpo sale:", body.name)  # Verifica si otros cuerpos están saliendo
+		resetear_texturas()
+		print("Soltando la roca")
+
+func resetear_texturas():
+	if brazo_izquierdo: 
+		brazo_izquierdo.texture = textura_normal_BrazoIz
+	if brazo_derecho: 
+		brazo_derecho.texture = textura_normal_BrazoDr
