e9e74d243e8dcca7afc8a7cf20c89299c7a285c9
Igg2000 <ignaciogarcia200@gmail.com>
2025-03-09 18:54:00 +0100

    V 0.2.7
    He agregado un reloj de arena al tiempo del mapa 1

--- scripts/mapa_1.gd
+++ scripts/mapa_1.gd
@@ -1,10 +1,16 @@
 extends Node2D
 
 @onready var timer: Timer = $Timer
+@onready var reloj: TextureProgressBar = $Timer/CanvasLayer/RelojDeArena
 @onready var tiempo: RichTextLabel = $Timer/CanvasLayer/tiempo
 @export var tiempo_del_nivel: int = 60
 var record_actual: int  = 0
 var record_maximo: int
+
+var imagenReloj: int = 1 # 1 verde, 2 amarillo 3 rojo
+@onready var textura_reloj_1 = load("res://assets/sandwatch/reloj_de_arena1.png")
+@onready var textura_reloj_2 = load("res://assets/sandwatch/reloj_de_arena2.png")
+@onready var textura_reloj_3 = load("res://assets/sandwatch/reloj_de_arena3.png")
 
 
 # Called when the node enters the scene tree for the first time.
@@ -15,11 +21,18 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	#contador
 	tiempo.text = str(int(timer.time_left))
 	if timer.time_left < 11:
 		tiempo.text = "[color=red]"+str(int(timer.time_left))+"[/color]"
 	else:
 		tiempo.text = str(int(timer.time_left))
+		
+	#reloj de arena
+	reloj.value = int(timer.time_left)
+	
+	relojCambiaDeColor()
+	
 
 
 func _on_jugador_ha_llegado_a_meta() -> void:
@@ -50,7 +63,6 @@ func _cargar_record():
 		_guardar_record()
 	
 	
-	
 
 func _on_repetir_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/mapa1.tscn")
@@ -62,3 +74,24 @@ func _on_volver_pressed() -> void:
 
 func _on_timer_timeout() -> void:
 	$PantallaDerrota.show()
+	
+func relojCambiaDeColor():
+	
+	if reloj.value > tiempo_del_nivel / 2: #si queda mas de la mitad el reloj es verde
+		if imagenReloj == 1:
+			return
+		else:
+			reloj.texture_progress = textura_reloj_1
+			imagenReloj=1
+	elif reloj.value <= tiempo_del_nivel / 4: # si queda un cuarto o menos es rojo
+		if imagenReloj == 3:
+			return
+		else:
+			reloj.texture_progress = textura_reloj_3
+			imagenReloj = 3
+	else: #si llega aqui esta por debajo de la mitad y por encima del cuarto, y estaría en ambar
+		if imagenReloj == 2:
+			return;
+		else:
+			reloj.texture_progress = textura_reloj_2
+			imagenReloj=2
